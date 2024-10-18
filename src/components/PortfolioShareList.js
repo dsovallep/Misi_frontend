d4e50839791aabf716bbe103a58b41f8a2956b62
dsovallep <ds@athento.com>
2024-10-17 20:06:56 -0500

    Feature: Current stock value input is added to calculate profitability

--- src/components/PortfolioShareList.js
+++ src/components/PortfolioShareList.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { getPortfolioShare } from '../services/api_misi_back.js';
-import { Card, CardContent, Typography, List, ListItem, Divider } from '@mui/material';
+import { Card, CardContent, Typography, List, ListItem, Divider, TextField } from '@mui/material';
 
 
 const PortfolioShareList = () => {
     const [portShares, setPortShares] = useState([]);
+    const [currentPrices, setCurrentPrices] = useState({});
 
     const fetchPortfolioShare = async () => {
         try {
             const response = await getPortfolioShare();
-            console.log('response.data', response.data)
             setPortShares(response.data);
         } catch (error) {
             console.error('Error fetching data from PortfolioShareList: ', error);
@@ -19,6 +19,25 @@ const PortfolioShareList = () => {
     useEffect(() => {
         fetchPortfolioShare();
     }, []);
+
+    // Handling the change in the current price value 
+    const handlePriceChange = (id, value) => {
+        setCurrentPrices({
+            ...currentPrices,
+	    [id]: parseFloat(value) || 0,
+	});
+    };
+    
+    const calculateProfitability = (averagePrice, numberShare, currentPrice) => {
+        return (currentPrice - averagePrice) * numberShare;    
+    };
+
+    const formattedNumber = (number) => {
+        return Number(number).toLocaleString('es-ES', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+	});
+    }
 
     return (
         <Card>
@@ -32,12 +51,30 @@ const PortfolioShareList = () => {
                             <ListItem>
 			        <Typography variant="body1">
                                     <strong>share name:</strong> {ps.share_name} <br/> 
-                                    <strong>number share:</strong> {ps.number_share} <br/> 
-                                    <strong>amount:</strong> {ps.amount} <br/> 
-                                    <strong>average price per share:</strong> {ps.average_price_per_share} <br/> 
-                                    <strong>profit/loss:</strong> {ps.profit_loss} <br/> 
-                                    <strong>total in fees:</strong> {ps.total_in_fees} <br/>
-                                </Typography>
+                                    <strong>number share:</strong> {formattedNumber(ps.number_share)} <br/> 
+                                    <strong>amount:</strong> {formattedNumber(ps.amount)} <br/> 
+                                    <strong>average price per share:</strong> {formattedNumber(ps.average_price_per_share)} <br/> 
+                                    <strong>profit/loss:</strong> {formattedNumber(ps.profit_loss)} <br/> 
+                                    <strong>total in fees:</strong> {formattedNumber(ps.total_in_fees)} <br/>
+			            
+			            <TextField
+			                label="Current Price"
+			                variant="outlined"
+			                size="small"
+			                type="number"
+			                value={currentPrices[ps.id] || ''} // Display the current valur or empty if none has
+                                        onChange={(e) => handlePriceChange(ps.id, e.target.value)}
+			                style={{ marginTop: '10px' }}
+			            />
+			            
+			            <Typography variant="body2" color="primary" style={{ marginTop: '10px' }}>
+			                <strong>Calculated profitability:</strong> {
+                                            currentPrices[ps.id]
+						? formattedNumber(calculateProfitability(ps.average_price_per_share, ps.number_share, currentPrices[ps.id]))
+						: 'Enter current price to calculate'
+					}
+			            </Typography>
+			        </Typography>
 			    </ListItem>
 			    <Divider />
 			</React.Fragment>
